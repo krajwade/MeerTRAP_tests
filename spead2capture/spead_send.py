56c017ca2318b0e27704fafd13e031ac2bb65d7a
kmrajwade <rkaustubh10@gmail.com>
2018-06-01 12:12:49 +0100

    changed the python sender to mimic the fbfuse packets

--- spead2capture/spead_send.py
+++ spead2capture/spead_send.py
@@ -10,22 +10,32 @@ import sys
 import datetime
 
 
+# Set up packet transfer based on arguments
+
+if (len(sys.argv) != 7):
+    print("Incorrect number of arguments")
+    print ("Correct Usage: python spead2_send.py <nheaps> <nchans> <ntime> <nsamples> <nfreq> <nbeams>")
+    exit()
+
 # Setup the transfer
 
-nchans = 4096
-ntime = 64
-nsamples = 128
-nbeams = 6
+nchans = int(sys.argv[2])
+ntime = int(sys.argv[3])
+nsamples = int(sys.argv[4])
+nfreq = int(sys.argv[5])
+nbeams = int(sys.argv[6])
 
-print 'TX: Initializing...'
+print ('TX: Initializing...')
 
-# send 6 beams and full band
+ind = 0
+# Send nbeams beams
+
 for i in range(nbeams):
 
   tx = spead.Transmitter(spead.TransportUDPtx('127.0.0.1', 8888, rate=1e3))
 
-# Send 100 heaps
-  for j in range(100):
+# Send N heaps
+  for j in range(int(sys.argv[1])):
 
     ig = spead.ItemGroup()
 
@@ -53,14 +63,26 @@ for i in range(nbeams):
     ig.add_item(name='frequency', description='frequency channel number of the first channel',
             shape=[], fmt=spead.mkfmt(('u',32)),
                                       init_val=(0))
-    ig['frequency'] = 4096
+    # cycle through the frequency channels in steps of 128, 32 times
+    
+    ig['frequency'] = 128*ind
+    if (ind == 32):
+     ind = 0
+    else:
+     ind +=1
 
+#nchans
+
+    ig.add_item(name='nchans', description='Number of channels per heap',
+            shape=[], fmt=spead.mkfmt(('u',32)),
+                                       init_val=(0))
+    ig['nchans'] = 128
 
 # data
 
-    data = np.arange(4096*64*128).astype(np.uint32); data.shape = (4096,64,128)
+    data = np.arange(128*64*128).astype(np.uint32); data.shape = (128,64,128)
     ig.add_item(name='data', description='Beamformed data for the given beam id',
-              shape=[4096,64,128], ndarray=data)
+              shape=[128,64,128], ndarray=data)
     ig['data'] = data
 
 # send the heap
@@ -71,4 +93,4 @@ for i in range(nbeams):
 
 # Done!
 
-print "trasmission done!"
+print ("trasmission done!")
